63871e4a3067977a55d477185cc42f82e744f0f4
elias10 <eliasse@discovery.co.za>
2020-06-04 19:17:13 +0200

    Initial Commit

---
+++ src/main/java/co/za/besolutions/Calculator.java
@@ -0,0 +1,8 @@
+package co.za.besolutions;
+
+public class Calculator {
+
+    public int add(int a,int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/co/za/besolutions/CalculatorTest.java
@@ -0,0 +1,10 @@
+package co.za.besolutions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void shouldAddTwoNumbers() {
+    }
+}
