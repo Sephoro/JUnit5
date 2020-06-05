a4df82c3032bf3432fd7685bc2de4bdf6aeaf15a
elias10 <eliasse@discovery.co.za>
2020-06-05 11:58:32 +0200

    Assertions with messages

--- src/test/java/co/za/besolutions/CalculatorTest.java
+++ src/test/java/co/za/besolutions/CalculatorTest.java
@@ -1,10 +1,19 @@
 package co.za.besolutions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     @Test
     void shouldAddTwoNumbers() {
+
+        Calculator calculator = new Calculator();
+
+        int expected = 10;
+        int actual = calculator.add(5,5);
+
+        assertEquals(expected, actual, "The add method should add two numbers");
     }
 }
