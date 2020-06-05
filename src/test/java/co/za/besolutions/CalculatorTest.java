cccbe0fb9735db0fd28811fe1c89336ed667a34d
elias10 <eliasse@discovery.co.za>
2020-06-05 18:17:31 +0200

    JUnit sentences and Nesting

--- src/test/java/co/za/besolutions/CalculatorTest.java
+++ src/test/java/co/za/besolutions/CalculatorTest.java
@@ -2,11 +2,14 @@ package co.za.besolutions;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assumptions.*;
+
+@DisplayName("When running Calculator")
 class CalculatorTest {
 
     private Calculator calculator;
@@ -16,50 +19,129 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
-    @Test
-    @DisplayName("Two numbers can be added")
-    void addTest() {
+    @Nested
+    @DisplayName("add method")
+    class AddTest{
 
-        double expected = 10;
-        double actual = calculator.add(5,5);
+        @Test
+        @DisplayName("when adding a number and zero")
+        void addToZeroTest() {
 
-        assertEquals(expected, actual, "The add method should add two numbers");
+            double expected = 5;
+            double actual = calculator.add(0,5);
+
+            assertEquals(expected, actual, "should return the number");
+        }
+
+        @Test
+        @DisplayName("when adding two positive numbers")
+        void addPositiveTest() {
+
+            double expected = 10;
+            double actual = calculator.add(5,5);
+
+            assertEquals(expected, actual, "should return the correct positive sum");
+        }
+
+        
+        @Test
+        @DisplayName("when adding a positive number and a negative number")
+        void addNegativeToPositiveTest() {
+
+            double expected = -5;
+            double actual = calculator.add(-10,5);
+
+            assertEquals(expected, actual, "should return the right sum");
+        }
+
+        @Test
+        @DisplayName("when adding two negative numbers")
+        void addNegativeTest() {
+
+            double expected = -15;
+            double actual = calculator.add(-10,-5);
+
+            assertEquals(expected, actual, "should return a negative correct sum");
+        }
+
+
     }
+    
+    @Nested
+    @DisplayName("multiply method")
+    class MultiplyTest{
 
-    @Test
-    @DisplayName("Two numbers can be multiplied")
-    void multiplyTest(){
+        @Test
+        @DisplayName("When multiplying by zero")
+        void multiplyByZeroTest(){
 
-        double expected = 10;
-        double actual = calculator.multiply(5,2);
-        assertEquals(expected, actual, "The multiply method returns the multiplication of two numbers");
+            double expected = 0;
+            double actual = calculator.multiply(50000,0);
+            assertEquals(expected, actual, "should return zero");
+        }
+
+        @Test
+        @DisplayName("When multiplying two Positive Numbers")
+        void multiplyPositiveTest(){
+
+            double expected = 15;
+            double actual = calculator.multiply(5,3);
+            assertEquals(expected, actual, "should return a positive correct product");
+        }
+
+        @Test
+        @DisplayName("When multiplying a negative number and a positive")
+        void multiplyPositiveAndNegativeTest(){
+
+            double expected = -15;
+            double actual = calculator.multiply(-5,3);
+            assertEquals(expected, actual, "should return a negative correct product");
+        }
+
+        @Test
+        @DisplayName("When multiplying two negative numbers")
+        void multiplyNegativeTest(){
+
+            double expected = 15;
+            double actual = calculator.multiply(-5,-3);
+            assertEquals(expected, actual, "should return a positive correct product");
+        }
+        
     }
+   
 
     @Test
-    @DisplayName("The Area of a Circle can be computed")
+    @DisplayName("The computeCircleAreaMethod, given a radius")
     void computeCircleAreaTest(){
 
         double expected = Math.PI*5*5;
         double actual = calculator.computeCircleArea(5);
-        assertEquals(expected, actual, "Should calculate the area of a circle");
+        assertEquals(expected, actual, "should return the correct area");
     }
+    
+    @Nested
+    @DisplayName("divide method")
+    class DivideTest{
 
-    @Test
-    @DisplayName("Dividing By Zero Throws DivideByZeroException")
-    void divideExceptionTest(){
+        @Test
+        @DisplayName("when dividing by zero")
+        void divideExceptionTest(){
 
-        assertThrows(Calculator.DivisionByZero.class, () -> calculator.divide(1,0), "Division by zero throws DivideByZeroException");
+            assertThrows(Calculator.DivisionByZero.class, () -> calculator.divide(1,0), "should throw DivideByZeroException");
+        }
+
+        @Test
+        @DisplayName("When dividing two numbers")
+        void divideTest() throws Calculator.DivisionByZero {
+
+            boolean isServerUp = true;
+
+            double expected = 2.5;
+            double actual = calculator.divide(5,2);
+            assumeTrue(isServerUp, "assuming the server is up");
+            assertEquals(expected, actual, "should return the correct quotient");
+        }
+        
     }
-
-    @Test
-    @DisplayName("Two Numbers can be divided")
-    void divideTest() throws Calculator.DivisionByZero {
-
-        boolean isServerUp = true;
-
-        double expected = 2.5;
-        double actual = calculator.divide(5,2);
-        assumeTrue(isServerUp, "The test must be executed only when the server is up");
-        assertEquals(expected, actual, "Two Numbers should be divided");
-    }
+   
 }
